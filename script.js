c4146a628dd02659935114a50b2e52bf1f724ac5
Karl Broman <kbroman@gmail.com>
2016-06-30 07:17:20 -0700

    get some of the test.csv data in there
    - confused about dates
    - also confused about how to handle the multiple columns

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var dF = new Date(2012,4,2)
+var dF = new Date(2015,1,1)
 var context = cubism.context()
     .serverDelay(Date.now() - dF) //correct sign so axis is correct & not in future.
-    .step(864e5)
+    .step(1280*60*60*24)
     .size(1280)
     .stop();
 
@@ -15,10 +15,10 @@ d3.select("body").append("div")
     .attr("class", "rule")
     .call(context.rule());
 
-var Data = ["AAPL", "BIDU"].map(stock);
-var primary = Data[1];
-var secondary = primary.shift(-864e5*30);
-Data[2] = secondary;
+var Data = ["test2"].map(stock);
+//var primary = Data[1];
+//var secondary = primary.shift(-864e5*30);
+//Data[2] = secondary;
 
 
 d3.select("body").selectAll(".horizon")
@@ -34,16 +34,20 @@ context.on("focus", function(i) {
 
 // Replace this with context.graphite and graphite.metric!
 function stock(name) {
-  var format = d3.time.format("%d-%b-%y");
+  var format = d3.time.format("%Y-%m-%d");
   return context.metric(function(start, stop, step, callback) {
     d3.csv(name + ".csv", function(rows) {
-      rows = rows.map(function(d) { return [format.parse(d.Date), +d.Open]; }).filter(function(d) { return d[1]; }).reverse();
-      var date = rows[0][0], compare = rows[400][1], value = rows[0][1], values = [value];
-      rows.forEach(function(d) {
-        while ((date = d3.time.day.offset(date, 1)) < d[0]) values.push(value);
-        values.push(value = (d[1] - compare) / compare);
+        rows = rows.map(function(d) { return [format.parse(d.date), +d.a]; })
+                   .filter(function(d) { return d[1]; }).reverse();
+
+        var dates = []
+        var values = [];
+
+        rows.forEach(function(d) {
+            dates.push(d[0])
+          values.push(d[1])
       });
-      callback(null, values.slice(-context.size()));
+        callback(null, values)
     });
   }, name);
 }
