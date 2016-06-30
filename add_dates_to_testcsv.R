f9cb90f4d3a46edc723d7ffaf6355ea75ab6afc8
Karl Broman <kbroman@gmail.com>
2016-06-30 06:56:58 -0700

    Add test data

---
+++ add_dates_to_testcsv.R
@@ -0,0 +1,11 @@
+# add dates to test.csv
+
+test <- read.csv("test.csv", stringsAsFactors=FALSE)
+
+dates <- seq(ymd("2015-01-01"), ymd("2016-06-30"), length.out=nrow(test))
+dates <- as.character(dates)
+test[,1] <- dates
+names(test)[1] <- "date"
+
+write.table(test, "test2.csv", sep=",",
+            row.names=FALSE, col.names=FALSE, quote=FALSE)
