a1f3dcc0e9bac088b522dc32981eb1de6e15c3d4
Karl Broman <kbroman@gmail.com>
2016-06-30 07:02:59 -0700

    Fix add_dates_...R
     - forgot library(lubridate)
     - need col.names=TRUE

--- add_dates_to_testcsv.R
+++ add_dates_to_testcsv.R
@@ -2,10 +2,11 @@
 
 test <- read.csv("test.csv", stringsAsFactors=FALSE)
 
+library(lubridate)
 dates <- seq(ymd("2015-01-01"), ymd("2016-06-30"), length.out=nrow(test))
 dates <- as.character(dates)
 test[,1] <- dates
 names(test)[1] <- "date"
 
 write.table(test, "test2.csv", sep=",",
-            row.names=FALSE, col.names=FALSE, quote=FALSE)
+            row.names=FALSE, col.names=TRUE, quote=FALSE)
